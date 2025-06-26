d950d4efefa06908dc18faee4f131332f4334011
ShobhitChola <shobhitchola1@gmail.com>
2025-06-26 23:19:17 +0530

    build: Final fixes for Next.js 14 compatibility

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-    transpilePackages: [
+  transpilePackages: [
     'antd',
     'rc-util',
     'rc-pagination',
